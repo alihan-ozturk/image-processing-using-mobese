4d6c3cc9052b99effee6701fcea2370727fc7388
Alihan <72665972+alihan-ozturk@users.noreply.github.com>
2023-03-06 15:36:34 +0300

    Update ffmpgProcess.py

--- ffmpgProcess.py
+++ ffmpgProcess.py
@@ -1,5 +1,4 @@
 import subprocess as sp
-import time
 import cv2
 import numpy as np
 import datetime
@@ -43,31 +42,31 @@ while c:
     for cameraName, cameraPath, cameraSize in zip(cameraNames, cameraPaths, cameraSizes):
         VIDEO_URL = WEBURL + cameraPath
         pipe = sp.Popen([FFMPEG_BIN, "-i", VIDEO_URL,
-                         "-loglevel", "quiet",  # no text output
-                         "-an",  # disable audio
+                         "-loglevel", "quiet",
+                         "-an",
                          "-f", "image2pipe",
                          "-pix_fmt", "bgr24",
                          "-vcodec", "rawvideo", "-"],
                         stdin=sp.PIPE, stdout=sp.PIPE)
         size = cameraSize[0] * cameraSize[1] * cameraSize[2]
         for i in range(101):
-            if i % 100 == 0:
-                now = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-                raw_image = pipe.stdout.read(size)
-                try:
-                    image = np.frombuffer(raw_image, dtype='uint8').reshape(cameraSize)
-                except ValueError:
-                    print(cameraName, now)
+            # if i % 100 == 0:
+            now = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+            raw_image = pipe.stdout.read(size)
+            try:
+                image = np.frombuffer(raw_image, dtype='uint8').reshape(cameraSize)
+            except ValueError:
+                print(cameraName, now)
                 # imgName = path + cameraName + now + ".png"  # x
                 # cv2.imwrite(imgName, image)  # x
-                cv2.imshow("test", image)
-                key = cv2.waitKey(70)
-                if key == 27:
-                    cv2.destroyAllWindows()
-                    c = False
-                    break
-                elif key == ord("q"):
-                    break
+            cv2.imshow("test", image)
+            key = cv2.waitKey(70)
+            if key == 27:
+                cv2.destroyAllWindows()
+                c = False
+                break
+            elif key == ord("q"):
+                break
         # time.sleep(100)  # x
         if c == 0:
             break
