83811e3fabcae59db946890c73d760b0fd07bed8
Alihan <72665972+alihan-ozturk@users.noreply.github.com>
2023-07-11 11:33:14 +0300

    Update streamVideo.py

--- streamVideo.py
+++ streamVideo.py
@@ -2,6 +2,7 @@ import time
 import threading
 import subprocess as sp
 import numpy as np
+import cv2
 
 
 class CustomThread(threading.Thread):
@@ -34,3 +35,19 @@ class CustomThread(threading.Thread):
     def stop(self):
         self.c = False
         print("stopped")
+
+
+def letterbox(img, new_shape=(640, 640), color=(114, 114, 114), auto=True, scaleFill=False, scaleup=True, stride=32):
+    shape = img.shape[:2]
+    r = min(new_shape[0] / shape[0], new_shape[1] / shape[1])
+    r = min(r, 1.0)
+
+    new_unpad = int(round(shape[1] * r)), int(round(shape[0] * r))
+    dw, dh = new_shape[1] - new_unpad[0], new_shape[0] - new_unpad[1]  # wh padding
+    dw, dh = np.mod(dw, stride) / 2, np.mod(dh, stride) / 2  # wh padding
+
+    img = cv2.resize(img, new_unpad, interpolation=cv2.INTER_LINEAR)
+    top, bottom = int(round(dh - 0.1)), int(round(dh + 0.1))
+    left, right = int(round(dw - 0.1)), int(round(dw + 0.1))
+    img = cv2.copyMakeBorder(img, top, bottom, left, right, cv2.BORDER_CONSTANT, value=color)  # add border
+    return img
